7fbd547ee8e3d4121dfd1c57622cf50e1a8db3fa
Lucas Vidmar <lucas@vidmar.xyz>
2023-06-07 23:25:29 -0300

    exponencial compleja

---
+++ TP1/exponencial-compleja-continuo.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# --- INGRESO ---
+
+t0 = -1 # tiempo inicial
+tn = 1 # tiempo final
+n = 100 # cantidad de muestras
+
+# --- PROCEDIMIENTO ---
+
+# vector de tiempo
+dt = (tn-t0)/n # intervalo de tiempo
+ti = np.arange(t0,tn,dt) # vector de tiempo
+
+# Senal
+senal = np.exp(1j*2*np.pi*ti) # e^(j*2*pi*t)
+
+# Parte real
+x = [ele.real for ele in senal]
+# Parte imaginaria
+y = [ele.imag for ele in senal]
+
+# Imprimo
+np.set_printoptions(precision = 4)
+print('tiempo: ')
+print(ti)
+print('señal: x(t) ')
+print(senal)
+
+# Gráfica
+plt.plot(x,y, color="red", marker='.')
+plt.axhline(0, color='gray')
+plt.axvline(0, color='gray')
+plt.ylabel('Imaginario')
+plt.xlabel('Real')
+plt.show()
---
+++ TP1/exponencial-compleja-discreto.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# --- INGRESO ---
+
+n0 = -10 # discreto inicial
+m = 20 # cantidad de muestras
+
+# --- PROCEDIMIENTO ---
+
+# vector n discreto [n0,n0+m]
+n = np.arange(n0,n0 + m,1)
+
+# Senal
+senal = np.exp(1j*np.pi*n/10) # e^(j*pi*n/10)
+
+# Parte real
+x = [ele.real for ele in senal]
+# Parte imaginaria
+y = [ele.imag for ele in senal]
+
+
+# Imprimo
+np.set_printoptions(precision=4)
+print('n: ')
+print(n)
+print('señal x[n]: ')
+print(senal)
+
+# Gráfica
+plt.stem(x, y)
+plt.axhline(0, color='gray')
+plt.axvline(0, color='gray')
+plt.ylabel('Imaginario')
+plt.xlabel('Real')
+plt.show()
