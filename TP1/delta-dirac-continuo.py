e04ef4aa4934f7eede2cd8e34465c541fccd1d9a
Lucas Vidmar <lucas@vidmar.xyz>
2023-06-07 22:40:37 -0300

    delta dirac

---
+++ TP1/delta-dirac-continuo.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# --- FUNCIONES ---
+def ddf(x,sig):
+    if -(1/(2*sig)) <= x and x <= (1/(2*sig)):
+        val = sig
+    else:
+        val = 0
+    return val
+
+# --- INGRESO ---
+
+t0 = -1 # tiempo inicial
+tn = 1 # tiempo final
+n = 1000 # cantidad de muestras
+
+# --- PROCEDIMIENTO ---
+
+# vector de tiempo
+dt = (tn-t0)/n # intervalo de tiempo
+ti = np.arange(t0,tn,dt) # vector de tiempo
+
+# Senal
+senal = [ddf(t, 100) for t in ti]
+
+# Imprimo
+np.set_printoptions(precision = 4)
+print('tiempo: ')
+print(ti)
+print('señal: x(t) ')
+print(senal)
+
+# Gráfica
+plt.plot(ti,senal, color="red")
+plt.axhline(0, color='gray')
+plt.axvline(0, color='gray')
+plt.xlabel('t')
+plt.ylabel('señal x[(t)]')
+plt.show()
---
+++ TP1/delta-dirac-discreto.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# --- FUNCIONES ---
+def ddf(n):
+    return 1 if n == 0 else 0
+
+# --- INGRESO ---
+
+n0 = -1 # tiempo inicial
+m = 3 # cantidad de muestras
+
+# --- PROCEDIMIENTO ---
+
+# vector n discreto [n0,n0+m]
+n = np.arange(n0,n0 + m,1)
+
+# señal
+senal = [ddf(nk) for nk in n]
+
+# Imprimo
+np.set_printoptions(precision=4)
+print('n: ')
+print(n)
+print('señal x[n]: ')
+print(senal)
+
+# Gráficas
+plt.stem(n, senal)
+plt.xlabel('n')
+plt.ylabel('señal x[n]')
+plt.show()
