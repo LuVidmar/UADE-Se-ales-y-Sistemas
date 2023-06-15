f3aa07785f4555fd283a7c6857a6c9ca647d0f84
Lucas Vidmar <lucas@vidmar.xyz>
2023-06-14 21:34:06 -0300

    senal energia

---
+++ TP1/senal-energia.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def square(t):
+    return np.heaviside(t,1) - np.heaviside(t-1,1)
+
+# --- INGRESO ---
+
+t0 = -20 # tiempo inicial
+tn = 20 # tiempo final
+n = 1000 # cantidad de muestras
+
+# --- PROCEDIMIENTO ---
+
+# vector de tiempo
+dt = (tn-t0)/n # intervalo de tiempo
+ti = np.arange(t0,tn,dt) # vector de tiempo
+
+# Senal
+senal = square(ti/10+1/2)
+
+# Imprimo
+np.set_printoptions(precision = 4)
+print('tiempo: ')
+print(ti)
+print('señal: x(t) ')
+print(senal)
+
+# Gráfica
+plt.axhline(0, color='gray')
+plt.axvline(0, color='gray')
+plt.plot(ti,senal, color="red")
+plt.xlabel('t')
+plt.ylabel('señal x[(t)]')
+plt.show()
