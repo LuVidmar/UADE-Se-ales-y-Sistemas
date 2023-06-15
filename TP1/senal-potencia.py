72ca118ffe9e693d82024238c0f4160b7b0ad780
Lucas Vidmar <lucas@vidmar.xyz>
2023-06-14 21:27:05 -0300

    senal potencia

---
+++ TP1/senal-potencia.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def square(t):
+    return np.heaviside(t,1) - np.heaviside(t-1,1)
+
+# --- INGRESO ---
+
+t0 = -20 # tiempo inicial
+tn = 20 # tiempo final
+n = 1000 # cantidad de muestras
+
+# --- PROCEDIMIENTO ---
+
+# vector de tiempo
+dt = (tn-t0)/n # intervalo de tiempo
+ti = np.arange(t0,tn,dt) # vector de tiempo
+
+# Senal
+senal = np.zeros(len(ti)) # vector de ceros
+for i in range(t0, tn, 5):
+    senal += square(ti-i) # suma de señales cuadradas
+
+# Imprimo
+np.set_printoptions(precision = 4)
+print('tiempo: ')
+print(ti)
+print('señal: x(t) ')
+print(senal)
+
+# Gráfica
+plt.axhline(0, color='gray')
+plt.axvline(0, color='gray')
+plt.plot(ti,senal, color="red")
+plt.xlabel('t')
+plt.ylabel('señal x[(t)]')
+plt.show()
