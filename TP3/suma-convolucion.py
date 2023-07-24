f1239a676f1ca52c50aa9b323e5f3cbd7bd6c2fd
NizyBoxer03 <nizyboxer03@gmail.com>
2023-07-24 00:56:46 -0300

    bruh

--- TP3/suma-convolucion.py
+++ TP3/suma-convolucion.py
@@ -19,40 +19,21 @@ f = lambda t: square(t)
 g = lambda t: np.exp(-t+1) * np.heaviside(t, 1)
 
 # Defino el tiempo
-m = 10 - 1 # muestras por división
-dn = 1/m
+m = 25 # muestras por división
+dn = 1/(m-1)
 
 inicio = -5
 fin = 5
 
 n = np.arange(inicio, fin + dn, dn)
 
-y  = np.convolve(f(n), g(n), 'same') * (dn)
-'''
-# Grafico
-fig, ax1 = plt.subplots(figsize=(8, 8))
-ax2 = ax1.twinx()
+y  = np.convolve(f(n), g(n), 'same') * dn
 
-ax1.axvline(0, color='gray')
-ax1.axhline(0, color='gray')
-
-ax1.plot(t, f(t), label='f(t)', color='red')
-ax1.plot(t, g(t), label='g(t)', color='blue')
-ax2.plot(t, y, label='y(t) = {f*g}(t)', color='green')
-
-ax1.set_xlabel("t")
-ax1.set_ylabel("f,g")
-ax1.tick_params(axis="y")
-ax2.set_ylabel("y")
-ax2.tick_params(axis="y")
-
-plt.tight_layout()
-
-plt.axvline(x=0.5, ls='--')
-
-fig.legend()
-plt.show()
-'''
+# Imprimo valores
+if len(sys.argv) > 1:
+    if sys.argv[1] != 'no-print':
+        print('y(t)')
+        print(y)
 
 fig, ax1 = plt.subplots(figsize=(8, 8))
 ax2 = ax1.twinx()
