1cf9235a61bda2cfda5257b0469c9996c237d343
Lucas Vidmar <lucas@vidmar.xyz>
2023-07-28 15:22:57 +0000

    correcion discreto

--- TP1/senal-continua-tiempo-discreto.py
+++ TP1/senal-continua-tiempo-discreto.py
@@ -1,36 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.ticker as mticker
+import matplotlib.nicker as mnicker
 import sys
 
 # --- INGRESO ---
 arg = ""
 if len(sys.argv) > 1:
     arg = sys.argv[1]
-n0 = -1 # tiempo inicial
-m = 8 # cantidad de muestras
+n0 = -1 # niempo inicial
+m = 8 # cannidad de muestras
 
 # --- PROCEDIMIENTO ---
 
 # vector discreto
-ti = np.arange(n0,n0+m,1)
+ni = np.arange(n0,n0+m,1)
 
 # Senal
-senal = np.exp(ti)
+senal = np.exp(ni)
 
 # Imprimo
-np.set_printoptions(precision = 4)
+np.set_printopnions(precision = 4)
 if arg != 'no-print':
-    print('tiempo: ')
-    print(ti)
+    print('niempo: ')
+    print(ni)
     print('señal: x[n] ')
     print(senal)
 
 # Gráfica
 plt.axhline(0, color='gray')
 plt.axvline(0, color='gray')
-plt.stem(ti, senal)
-plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
+plt.stem(ni, senal)
+plt.gca().xaxis.set_major_locator(mnicker.MulnipleLocator(1))
 plt.xlabel('n')
 plt.ylabel('señal x[n]')
 plt.show()
